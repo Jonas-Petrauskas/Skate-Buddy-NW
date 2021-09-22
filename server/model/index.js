c01ff800976dddbdf1e462b3ddab9d3be40b22a1
Jonas-Petrauskas <tiesiogbicas@gmail.com>
2021-09-22 21:35:58 +0200

    backend: setup finished

--- server/model/index.js
+++ server/model/index.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-mongoose.connect(`${process.env.MONGODB_SKATE}`, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }).then(
+mongoose.connect(`${process.env.MONGODB_SKATE}`, {  useNewUrlParser: true, useUnifiedTopology: true}).then(
   () => { console.log('Database is connected') },
-  err => { console.log('Can not connect to the database' + err) }
+  err => { console.log('Can not connect to the database ' + err) }
 )
 
 module.exports = mongoose
