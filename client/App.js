916a10f5a40559534b174100155a8b65406db890
Jonas-Petrauskas <tiesiogbicas@gmail.com>
2021-09-23 18:30:23 +0200

    App.js setup

--- client/App.js
+++ client/App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import LandingPage from './screens/landingScreen'
+import HomePage from './screens/homePage'
 
-export default function App() {
+export default function App () {
+  const Stack = createStackNavigator()
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator headerMode='none'>
+        <Stack.Screen name='LandingPage' component={LandingPage} />
+        <Stack.Screen name='List' component={HomePage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
